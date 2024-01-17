ac223d884eb00b6bd653802925c2f1b543bf2eb0
ernisto <joaovitordesouza707@gmail.com>
2024-01-17 18:47:16 -0300

    currency system

---
+++ src/server/Player/Currency/init.lua
@@ -0,0 +1,30 @@
+--// Packages
+local Entity = require(game.ReplicatedStorage.Packages.Entity)
+local Booster = require(game.ServerScriptService.Booster)
+local PlayerProfile = require(script.Parent.Profile)
+
+--// Data
+local awaitData = PlayerProfile.subData('Currency', {
+    amount = 0
+})
+
+--// Trait
+return Entity.trait('PlayerCurrency', function(self, player: Player)
+    
+    --// Data
+    local data = awaitData(player)
+    self:_syncAttributes(data)
+    
+    self.boost = self:_host(Booster.new('moneyBooster'))
+    
+    --// Methods
+    function self:add(amount: number)
+        
+        self.amount += amount
+    end
+    function self:consume(amount: number)
+        
+        assert(self.amount >= amount, `not enough money`)
+        self.amount -= amount
+    end
+end)
