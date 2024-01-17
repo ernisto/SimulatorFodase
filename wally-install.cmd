b06210d1b57bd524badea215c0fec103fa693cb3
ernisto <joaovitordesouza707@gmail.com>
2024-01-16 21:05:49 -0300

    added wally installer for DevDeps

--- wally-install.cmd
+++ wally-install.cmd
@@ -1,4 +1,5 @@
 wally install
 rojo sourcemap -o sourcemap.json
 wally-package-types --sourcemap sourcemap.json Packages/
+wally-package-types --sourcemap sourcemap.json DevPackages/
 wally-package-types --sourcemap sourcemap.json ServerPackages/
