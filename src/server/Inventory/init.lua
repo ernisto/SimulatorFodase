7dd10fa61627a22864bb9b1ebe4d781c429cb32b
ernisto <joaovitordesouza707@gmail.com>
2024-01-18 16:58:39 -0300

    update inventory item owning

--- src/server/Inventory/init.lua
+++ src/server/Inventory/init.lua
@@ -17,11 +17,29 @@ return Entity.trait('Inventory', function(self, entity: Instance)
     function self:removeItem(item: Item)
         
         item.roblox.Parent = nil
-        self.items[item] = nil
     end
     function self:addItem(item: Item)
         
-        item.roblox.Parent = self.roblox
-        self.items[item] = true
+        self:_host(item)
     end
+    
+    --// Listeners
+    self.roblox.ChildAdded:Connect(function(entity)
+        
+        local item = Item.find(entity)
+        if not item then return end
+        
+        self.itemAdded:_emit(item)
+        self.items[item] = true
+    end)
+    self.roblox.ChildRemoved:Connect(function(entity)
+        
+        local item = Item.find(entity)
+        if not item then return end
+        
+        if not self.items[item] then return end
+        self.items[item] = nil
+        
+        self.itemRemoved:_emit(item)
+    end)
 end)
