c887dd71310d4a1e653d3d5cc691cf3c0a5a02ff
ernisto <joaovitordesouza707@gmail.com>
2024-01-18 15:55:36 -0300

    parseAttributes give just the values of template

--- src/config/ParseAttributes.lua
+++ src/config/ParseAttributes.lua
@@ -1,14 +1,15 @@
 --// Module
 return function<config>(container: Instance, baseConfig: config, label: string?): config
     
-    local config = container:GetAttributes()
+    local attributes = container:GetAttributes()
+    local config = table.clone(baseConfig :: any)
     
-    for index, baseValue in baseConfig do
+    for index, baseValue in baseConfig :: any do
         
-        if typeof(config[index]) == typeof(baseValue) then continue end
-        
-        warn(`invalid {label or container:GetFullName()}, setting '{index}' (type {typeof(baseValue)})`)
-        config[index] = baseValue
+        local attribute = attributes[index]
+        if typeof(attribute) == typeof(baseValue) then config[index] = attribute
+        else warn(`invalid {label or container:GetFullName()}, setting '{index}' (type {typeof(baseValue)})`)
+        end
     end
     
     return config
