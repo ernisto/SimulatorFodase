ae9634e57c86c914a44a9cdf18dd0dac84de947b
ernisto <joaovitordesouza707@gmail.com>
2024-01-18 21:20:27 -0300

    adapt notifcation module

--- src/client/notify.lua
+++ src/client/notify.lua
@@ -2,7 +2,7 @@
 local Players = game:GetService("Players")
 local localPlayer = Players.LocalPlayer
 local localGui = localPlayer:WaitForChild("PlayerGui")
-local notificationsHololer = localGui:WaitForChild("HUD"):WaitForChild("Notifications")
+local notificationsHololer = localGui:WaitForChild("MainScreen"):WaitForChild("Notifications")
 
 --// Consts
 local DURATION = 5
@@ -10,7 +10,7 @@ local DURATION = 5
 --// Module
 return function(message: string, color: Color3)
 	
-	local notification = notificationsHololer.BaseText:Clone()
+	local notification = notificationsHololer.Notification:Clone()
 	notification.UIStroke.Color = Color3.new():Lerp(color, .5)
 	notification.TextColor3 = color
 	notification.Text = message
