43c1a1a787db8fdf6f26633223a4c4867e720c50
ernisto <joaovitordesouza707@gmail.com>
2024-01-23 16:07:48 -0300

    new UIKit

---
+++ src/shared/UIKit.lua
@@ -0,0 +1,26 @@
+--// Packages
+local Fusion = require(game.ReplicatedStorage.Shared.Fusion)
+local shortNumber = require(game.ReplicatedStorage.Shared.shortNumber)
+local shortTime = require(game.ReplicatedStorage.Shared.shortTime)
+
+--// Scope
+return Fusion:scoped{
+    watch = function(scope, instance, name: string)
+        
+        local canRead = pcall(function() return instance[name] end)
+        local output = scope:Value()
+        
+        scope:Hydrate(instance) {
+            [if canRead then Fusion.Out(name) else Fusion.AttributeOut(name)] = output
+        }
+        return output
+    end,
+    shortNumber = function(scope, input)
+        
+        return scope:Computed(function(use) return shortNumber(input) end)
+    end,
+    shortTime = function(scope, input)
+        
+        return scope:Computed(function(use) return shortTime(input) end)
+    end,
+}
--- src/shared/shortNumber.lua
+++ src/shared/shortNumber.lua
@@ -1,15 +1,15 @@
 local EXTENSIONS = {
-	'K', 'M', 'B', 'T', 'Q', 'Qn', 'Sx', 'Sp', 'Oc', 'Nn', 'Dc'
+    'K', 'M', 'B', 'T', 'Q', 'Qn', 'Sx', 'Sp', 'Oc', 'Nn', 'Dc'
 }
 return function(amount: number)
-	
-	if amount == 0 then return '0' end
-	if amount < 1 then return tostring(amount) end
-	
-	local cases = math.log10(amount)
-	local unit = cases // 3
     
-	return string.format("%.02f", amount/10^(unit*3))
-		:gsub("%.0+$", "")
-		.. (EXTENSIONS[unit] or "")
+    if amount == 0 then return '0' end
+    if amount < 1 then return string.format("%.2f", amount) end
+    
+    local cases = math.log10(amount)
+    local unit = cases // 3
+    
+    return string.format("%.02f", amount/10^(unit*3))
+        :gsub("%.0+$", "")
+        .. (EXTENSIONS[unit] or "")
 end
