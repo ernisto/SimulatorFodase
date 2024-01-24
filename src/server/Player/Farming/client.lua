2c9c01efde2a7b0cb8cecfafee47a14b94b02b6f
ernisto <joaovitordesouza707@gmail.com>
2024-01-24 14:32:16 -0300

    group auto click

--- src/server/Player/Farming/client.lua
+++ src/server/Player/Farming/client.lua
@@ -5,6 +5,9 @@ local Entity = require(game.ReplicatedStorage.Packages.Entity)
 local FarmingMarket = require(script.Parent.market)
 local PlayerFarming = require(script.Parent)
 
+--// Consts
+local GROUP_ID = 33720963
+
 --// Trait
 return Entity.trait('PlayerFarming', function(self, player: Player)
     
@@ -12,6 +15,7 @@ return Entity.trait('PlayerFarming', function(self, player: Player)
     local market = FarmingMarket.get(player)
     local client = Replicator.get(self.roblox)
     
+    --// Remotes
     function client.Click(player)
         
         farming:consumeCooldown()
@@ -19,7 +23,15 @@ return Entity.trait('PlayerFarming', function(self, player: Player)
     end
     function client.AutoClick(player, isToggled: boolean)
         
-        market.FastAutoClick:expect()
+        assert(self:canAutoClick(), `you havent auto clicker\njoin in the group or purchase one`)
         farming:toggleAutoclick(isToggled)
+    end
+    
+    --// Methods
+    function self:canAutoClick()
+        
+        return player:IsInGroup(GROUP_ID)
+            or market.FastAutoClick.isOwned
+            or market.MegaAutoClick.isOwned
     end
 end)
